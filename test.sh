a39992bf89d74ce12f523af3f525cf03ff15c461
cynthia iloekwe <cyniloekwe@gmail.com>
2021-01-12 21:26:42 +0100

    Install software for Ubuntu or debian distribution

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
+# Detect apt if Ubuntu or debian
+elif [ $(command -v apt-get) ]; then
+    apt-get update
+    apt-get install -y wget
+    apt-get install -y nodejs
+    apt-get install -y curl
+    exit 0
 fi
