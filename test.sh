34820c7d22d809dd951817c2cfd7f0aeb27b3268
cynthia iloekwe <cyniloekwe@gmail.com>
2021-01-12 21:27:39 +0100

    Install software fedora distribution

--- test.sh
+++ test.sh
@@ -5,4 +5,10 @@ elif [ $(command -v apt-get) ]; then
     apt-get install -y nodejs
     apt-get install -y curl
     exit 0
+# Detect dnf if fedora
+elif [ $(command -v dnf) ]; then
+    dnf install -y wget
+    dnf install -y nodejs
+    dnf install -y curl
+    exit 0
 fi
